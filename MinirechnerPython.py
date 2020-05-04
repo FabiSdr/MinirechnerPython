41c4fe733e52740d5455ebf84677ad16f04a1ad5
Fabian Sieder <02siefab@rgtfo-me.it>
2020-05-04 18:41:55 +0200

    Alle Grundrechnungsarten hinzugefügt

--- MinirechnerPython.py
+++ MinirechnerPython.py
@@ -1,3 +1,22 @@
 #Minirechner-Projekt in Python
 #Erstellt von Sieder Fabian
 
+Zahl1 = int(input("Zahl1: "))
+Zahl2 = int(input("Zahl2: "))
+
+def Add(Zahl1, Zahl2):
+    return Zahl1 + Zahl2
+
+def Sub(Zahl1, Zahl2):
+    return Zahl1 - Zahl2
+
+def Mul(Zahl1, Zahl2):
+    return Zahl1 * Zahl2
+
+def Div(Zahl1, Zahl2):
+    return Zahl1 / Zahl2
+
+print("Zahl1 + Zahl2 = " + str(Add(Zahl1,Zahl2)))
+print("Zahl1 - Zahl2 = " + str(Sub(Zahl1,Zahl2)))
+print("Zahl1 * Zahl2 = " + str(Mul(Zahl1,Zahl2)))
+print("Zahl1 / Zahl2 = " + str(Div(Zahl1,Zahl2)))
